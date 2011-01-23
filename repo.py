7ac2b6c540c3895eea5c492f13e69d1d56f6a804
Mayank Bhagya <mayankbhagya@gmail.com>
2011-01-24 02:52:35 +0530

    Added new README, repo.py, use_repo.py

--- repo.py
+++ repo.py
@@ -1,90 +1,69 @@
 #!/usr/bin/python2.6
 
-import crawler
 import time
 import os
 import pysvn
-import string
-import urllib2
-import urlparse
-
-svn_repos = 'file:///home/bhagya/Workspace/RadheshSir/work/repos/'
 
 class Repo:
-    base = ''
-    base_len = 0
-    dir_name = ''
-    def __init__ (self, baseurl):
-        url_comp = urlparse.urlparse (baseurl)
-        if url_comp.scheme == '':
-            baseurl = 'http://' + baseurl
-        self.base = baseurl
-        self.base_len = len (self.base)
-        url_comp = urlparse.urlparse (self.base)
-        self.dir_name = url_comp.netloc
+    svn_repo_url = ''
+    def __init__ (self, svn_repo_path):
+        svn_cli = pysvn.Client ()
+        if not os.path.isdir (svn_repo_path):
+            print "SVN repositories not found at the given location!"
+            quit ()
+        if svn_repo_path [-1:] != '/':
+            svn_repo_path = svn_repo_path + '/'
+        self.svn_repo_url = 'file://' + svn_repo_path
 
-    def init (self):
-
-        c = crawler.Crawler (self.base)
+    def init (self, dir_name, repo_name):
         svn_cli = pysvn.Client ()
 
-        if not os.path.isdir (self.dir_name):
-            os.mkdir (self.dir_name)
-
-        print 'CRAWLING...'
-        url_lst = c.crawl ()
-        print 'FETCHING...'
-        for url in url_lst:
-            try:
-                res = urllib2.urlopen (url)
-            except urllib2.URLError:
-                continue
-            print url
-            fname = self.dir_name + '/'  + string.replace (url,'/','_')
-            f = open (fname, 'w')
-            f.write (res.read ())
-            f.close ()
-
-        svn_cli.import_ (self.dir_name, svn_repos + self.dir_name, 'Initial import')
-        
-        print 'Repo created at ' + svn_repos + self.dir_name
-        print 'Working copy at ' + self.dir_name
-
-    def update (self):
-        c = crawler.Crawler (self.base)
-        svn_cli = pysvn.Client ()
-
-        if not os.path.isdir (self.dir_name):
-            os.mkdir (self.dir_name)
-
-        try:
-            svn_cli.checkout (svn_repos + self.dir_name, self.dir_name)
-        except pysvn._pysvn_2_6.ClientError:
-            print "Repository doesn't exist! Use init() to create one."
+        if not os.path.isdir (dir_name):
+            print "Directory doesn't exist! Can't init repository."
             quit ()
 
-        print 'CRAWLING...'
-        url_lst = c.crawl ()
-        print 'FETCHING...'
-        for url in url_lst:
-            try:
-                res = urllib2.urlopen (url)
-            except urllib2.URLError:
-                continue
-            print url
-            fname = self.dir_name + '/'  + string.replace (url,'/','_')
-            f = open (fname, 'w')
-            f.write (res.read ())
-            f.close ()
+        try:
+            svn_cli.import_ (dir_name, self.svn_repo_url + repo_name, 'Initial import')
+        except:
+            print 'SVN Error! Failed to import...'
+            quit ()
+
+        print 'Repo created at ' + self.svn_repo_url + dir_name
+        print 'Working copy at ' + dir_name
+
+    def update (self, dir_name, repo_name):
+        svn_cli = pysvn.Client ()
+
+        if not os.path.isdir (dir_name):
+            print "Directory doesn't exist! Can't update repository."
+            quit ()
 
         timestamp = time.asctime (time.localtime(time.time()))
-
-        svn_diff_text = svn_cli.diff('', self.dir_name)
-        if svn_diff_text is not '':
-            svn_cli.checkin (self.dir_name, timestamp)
-        else:
-            svn_diff_text = 'No change since last revision!'
+        try:
+            svn_cli.checkin (dir_name, timestamp)
+        except:
+            print "Can't update " + dir_name + '! SVN Error!'
+            quit ()
 
         print 'Updated Repo!'
-        print 'Updated files at ' + self.dir_name
+
+    def checkout (self, dir_name, repo_name):
+        svn_cli = pysvn.Client ()
+        if not os.path.isdir (dir_name):
+            try:
+                os.makedirs (dir_name)
+            except:
+                print 'Failed to create directory.'
+                quit()
+
+        svn_cli.checkout (self.svn_repo_url + repo_name, dir_name)
+
+    def diff (self, dir_name):
+        svn_cli = pysvn.Client ()
+        try:
+            svn_diff_text = svn_cli.diff('', dir_name)
+        except:
+            print 'SVN Error!'
+            quit ()
         return svn_diff_text
+
--- use_repo.py
+++ use_repo.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python2.6
 
 import repo
+import sys
 
-r = repo.Repo ('securityresearch.in/beta/wordpress')
-r.init ()
+if len (sys.argv) == 1:
+    print "Enter repository path setup by svnadmin!"
+    quit ()
+
+r = repo.Repo (sys.argv[1])
+#To be run only once for a repository
+#r.init ('/home/bhagya/Workspace/RadheshSir/work/test_repo', 'test_repo')
+
+#To checkout a repository, i.e. to create a working copy
+#r.checkout ('./test', 'test_repo')
+
+#To update a repository
+#r.update ('./test', 'test_repo')
+
+#To compare two repositories
+#t = r.diff ('./test')
 #print t
